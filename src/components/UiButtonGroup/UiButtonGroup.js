e97aba204b7f1ad5fe8fd4923c489a7c7115aed5
ThD.Rizer <thd.rizer@gmail.com>
2020-05-29 02:25:42 +0300

    Update UiButtonGroup.js

--- src/components/UiButtonGroup/UiButtonGroup.js
+++ src/components/UiButtonGroup/UiButtonGroup.js
@@ -64,7 +64,7 @@ export default {
       if (!defaultSlot) return null;
 
       // eslint-disable-next-line
-      console.log('[UiButtonGroup:genDefaultSlot] defaultSlot:', defaultSlot);
+      console.log('[UiButtonGroup:genDefaultSlot] defaultSlot:', defaultSlot());
 
       return defaultSlot().map(this.genButton);
     },
