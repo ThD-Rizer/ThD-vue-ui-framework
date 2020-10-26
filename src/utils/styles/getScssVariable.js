95d36ba501e12456ff11e8b5eca93ffe6915c5ec
ThD.Rizer <thd.rizer@gmail.com>
2020-10-26 03:05:00 +0300

    wip

--- src/utils/styles/getScssVariable.js
+++ src/utils/styles/getScssVariable.js
@@ -1,9 +1,10 @@
-import variables from '@/styles/export.scss';
+import variables from '../../styles/export.scss';
 
 /**
  * @param {String} variableKey Имя переменной
+ * @param {Object} dictionary Словарь переменных
  * @returns {Object}
  */
-export default function getScssVariable(variableKey) {
-  return variables[variableKey];
+export default function getScssVariable(variableKey, dictionary = variables) {
+  return dictionary[variableKey];
 }
