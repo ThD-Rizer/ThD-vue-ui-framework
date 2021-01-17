81c78f5494a97d5ad7b43a45465b82a38777c90f
ThD.Rizer <thd.rizer@gmail.com>
2021-01-17 08:29:22 +0000

    Release/0.27.9

--- src/utils/reverseArray.js
+++ src/utils/reverseArray.js
@@ -3,12 +3,12 @@ import { isArray } from './inspect';
 
 /**
  * @param {Array} array
- * @returns {Array} Cloned array
+ * @returns {Array} New reversed array
  */
-export default function cloneArray(array) {
+export default function reverseArray(array) {
   if (!isArray(array)) {
     throw new InvalidTypeError(array, 'array', 'Array');
   }
 
-  return [].concat(array);
+  return array.reduce((acc, element) => [element, ...acc], []);
 }
